44dbb6cdf09d4ee60f704f152e719093ffdd78fc
wegmatho <wegmatho@students.zhaw.ch>
2016-07-07 13:18:13 +0000

    dverses

--- server/models/apiModel.js
+++ server/models/apiModel.js
@@ -6,7 +6,7 @@ var apiSchema = Schema({
     description: { type: String, required: true},
     version: { type: Number, required: true},
     visibility: { type: [String], enum: ['CH-AXA', 'FR-AXA', 'CH-TWINT', 'PUBLIC'], required: true},
-    "auth-provider-type": { type: String, enum: ['OAuth', 'Mutual SSL', 'API-Key', 'Public'], required: true},
+    "auth-provider-type": { type: [String], enum: ['OAuth', 'Mutual SSL', 'API-Key', 'Public'], required: true},
     "swagger-doc": { type: String, required: false}
 });
 
