5793872686ecfb805f504af6a6eea67bd664a043
wegmatho <wegmatho@students.zhaw.ch>
2016-07-07 14:39:38 +0000

    div

--- server/models/tenantModel.js
+++ server/models/tenantModel.js
@@ -2,6 +2,7 @@ var mongoose = require("mongoose")
   , Schema = mongoose.Schema;
 
 var tenantSchema = Schema({
+    _id: Number,
     name: { type: String, enum: ['CH-AXA', 'FR-AXA', 'CH-TWINT', 'PUBLIC'], required: true},
     description: { type: String, required: true}
 });
