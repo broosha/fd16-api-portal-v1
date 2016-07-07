b64e957dfb6f4c6be9c613e5536d32e74abf0d7e
Mikhail <brodski@gmail.com>
2016-07-07 12:58:20 +0000

    models vervollständigt

---
+++ server/models/roleModel.js
@@ -0,0 +1,8 @@
+var mongoose = require("mongoose")
+  , Schema = mongoose.Schema;
+
+var roleSchema = Schema({
+    role-name: { type: String, enum: ['CH-AXA-User', 'CH-AXA-Admin', 'FR-AXA-User', 'FR-AXA-Admin', 'CH-TWINT-User', 'Admin'], required: true}
+});
+
+module.exports = mongoose.model('user', roleSchema);
