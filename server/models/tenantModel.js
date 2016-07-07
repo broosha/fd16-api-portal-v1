ce9eb03e024d96ab5090586c2b64657f726334c3
wegmatho <wegmatho@students.zhaw.ch>
2016-07-07 17:51:40 +0000

    angepasste models -> passendere ID-Attribute + AuthProvider direkt als ApiConsumer-Attribut

--- server/models/tenantModel.js
+++ server/models/tenantModel.js
@@ -2,8 +2,7 @@ var mongoose = require("mongoose")
   , Schema = mongoose.Schema;
 
 var tenantSchema = Schema({
-    _id: Number,
-    name: { type: String, enum: ['CH-AXA', 'FR-AXA', 'CH-TWINT', 'PUBLIC'], required: true},
+    _id: String, // name
     description: { type: String, required: true}
 });
 
